47cb64ea0686b2073cf750c13afc4efafdc14dcb
MounikaaSana <104676932+MounikaaSana@users.noreply.github.com>
2023-07-19 11:05:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function morning(){
+    document.getElementById("day").src="https://media.istockphoto.com/id/824800468/photo/sun-on-blue-sky-with-clouds.jpg?s=2048x2048&w=is&k=20&c=5No3BcKfNJFsYjjHzDJTtvmymIfXjvJKveF9ufOvEj0=";
+}
+function afternoon(){
+    document.getElementById("day").src="https://media.istockphoto.com/id/1226628621/photo/summer-background-orange-sky-with-clouds-and-bright-sun.jpg?s=2048x2048&w=is&k=20&c=N5Nkcvvh41O4WYiv9zf5Pez6CTB9GoB3DFMlf5_ysys=";
+}
+function night(){
+    document.getElementById("day").src="https://media.istockphoto.com/id/1200461833/photo/lens-flare-space-light-sun-light-abstract-black-background.jpg?s=2048x2048&w=is&k=20&c=qHY4v0tFt1fWtUXTL2blr5nXQbJdP4xE3cydFjek8m0=";
+}
